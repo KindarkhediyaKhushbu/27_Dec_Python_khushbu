4ad1e40ffcf64a9445b21efcd8f913a217bcdba9
khushbu kindarkhediya <khushbukindarkhediya@gmail.com>
2025-08-21 15:42:30 +0530

    Assement

---
+++ Assessment/MODULE -4/snippets_project/snippets/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import comment_list, comment_detail
+urlpatterns = [
+         path('comments/', comment_list),
+         path('comments/<int:pk>/', comment_detail),
+]
